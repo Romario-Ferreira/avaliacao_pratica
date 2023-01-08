60f8f5ca103ba8d1454c7c1c86a7f5a4b12d4619
Romário Ferreira <romarioffranca@gmail.com>
2023-01-08 13:02:16 -0300

    Project created

---
+++ src/main/java/com/francaemp/avaliacao_pratica/AvaliacaoPraticaApplication.java
@@ -0,0 +1,13 @@
+package com.francaemp.avaliacao_pratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoPraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoPraticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francaemp/avaliacao_pratica/AvaliacaoPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francaemp.avaliacao_pratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoPraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
